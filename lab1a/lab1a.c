93a4fd3018076cc7a0369751555c2a587e67c397
Zhouyu Qian <qzy@qzy.io>
2018-04-05 12:59:00 -0700

    Implement basic terminal I/O

--- lab1a/lab1a.c
+++ lab1a/lab1a.c
@@ -46,10 +46,60 @@ setup_term(void) {
   atexit(restore_term);
 }
 
+static ssize_t
+noeintr_write(int fd, const uint8_t *buf, size_t n) {
+  size_t bytes_left = n;
+  while (bytes_left > 0) {
+    ssize_t written = write(fd, buf, bytes_left);
+    if (written == -1) {
+      if (errno == EINTR) {
+        continue;
+      } else {
+        return -1;
+      }
+    }
+    buf += (size_t) written;
+    bytes_left -= (size_t) written;
+  }
+  return n;
+}
+
+static ssize_t
+noeintr_read(int fd, uint8_t *buf, size_t n) {
+  while (1) {
+    ssize_t bytes_read = read(fd, buf, n);
+    if (bytes_read == -1 && errno == EINTR) { continue; }
+    return bytes_read;
+  }
+}
+
+
+static void
+do_echo(void) {
+  while (1) {
+    uint8_t buf[1];
+    ssize_t bytes_read = noeintr_read(0, buf, 1);
+    if (bytes_read == 0) {
+      return; /* EOF somehow */
+    } else if (bytes_read == -1) {
+      fprintf(stderr, "%s: cannot read from standard input: %s\n", progname,
+              strerror(errno));
+      exit(1);
+    }
+    switch (*buf) {
+    case '\r': /* FALLTHROUGH */
+    case '\n': noeintr_write(1, (const uint8_t *) "\r\n", 2); break;
+    case 4: /* ^D */ return;
+    default: noeintr_write(1, buf, 1); break;
+    }
+  }
+}
+
 int
 main(int argc, char *argv[]) {
   progname = argv[0];
   (void) argc;
   setup_term();
+  do_echo();
   return 0;
 }
