461d6e242ef621021f529f1dd751c1e08ed17949
Zhouyu Qian <qzy@qzy.io>
2018-05-08 19:58:33 -0700

    Improve lab2_add_test to be faster and produce more relevant data

--- lab2a/lab2_add_test.sh
+++ lab2a/lab2_add_test.sh
@@ -4,21 +4,26 @@ rm -f ./lab2_add.csv
 
 set -eux
 
-for iterations in 100 1000 10000 100000; do
-    for threads in 2 4 8 12; do
+# No sync
+for iterations in 10 20 40 80 100 1000 5000 10000 20000 100000; do
+    for threads in 1 2 4 8 12; do
         for yieldopt in '' '--yield'; do
-            for syncopt in '' '--sync=m' '--sync=c'; do
-                ./lab2_add --iterations $iterations --threads $threads $yieldopt $syncopt >> ./lab2_add.csv
-            done
+            ./lab2_add --iterations $iterations --threads $threads $yieldopt >> ./lab2_add.csv
         done
     done
 done
 
-# spinlock special
-for iterations in 100 1000 ; do
-    for threads in 2 4; do
-        for yieldopt in '' '--yield'; do
-            ./lab2_add --iterations $iterations --threads $threads $yieldopt --sync=s >> ./lab2_add.csv
-        done
+# With yield, sync
+for threads in 2 4 8 12; do
+    for sync in m s c; do
+        iterations=$([ $sync = s ] && echo 1000 || echo 10000)
+        ./lab2_add --yield --iterations $iterations --threads $threads --sync $sync >> ./lab2_add.csv
+    done
+done
+
+# Without yield, sync
+for threads in 1 2 4 8 12; do
+    for syncopt in '' '--sync=s' '--sync=m' '--sync=c'; do
+        ./lab2_add --iterations 10000 --threads $threads $syncopt >> ./lab2_add.csv
     done
 done
